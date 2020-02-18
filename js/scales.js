fa37ba89142f3db8410291fe41810ac95040b9a0
Nicolas Tagliani <nicolas.tagliani@gmail.com>
2020-02-18 22:55:47 +0100

    Initial commit
    - Table visualizaion of an harmonica
    - Possibility to change the armonica key
    - incuded frequency analyzer from "Simple pitch detection" https://github.com/cwilso/PitchDetect

---
+++ js/scales.js
@@ -0,0 +1,61 @@
+{
+  ("use strict");
+
+  let notes = [
+    ["C"],
+    ["C#", "Db"],
+    ["D"],
+    ["D#", "Eb"],
+    ["E"],
+    ["F"],
+    ["F#", "Gb"],
+    ["G"],
+    ["G#", "Ab"],
+    ["A"],
+    ["A#", "Bb"],
+    ["B"],
+    ["C"],
+    ["C#", "Db"],
+    ["D"],
+    ["D#", "Eb"],
+    ["E"],
+    ["F"],
+    ["F#", "Gb"],
+    ["G"],
+    ["G#", "Ab"],
+    ["A"],
+    ["A#", "Bb"],
+    ["B"]
+  ];
+
+  function find_first_occurrence_of(key) {
+    var i = 0;
+    for (; i < 12; i++) {
+      for (var eq = 0; eq < notes[i].length; eq++) {
+        if (notes[i][eq] == key) {
+          return i;
+        }
+      }
+    }
+    return null;
+  }
+
+  function setup_scales(key) {
+    var start_index = find_first_occurrence_of(key);
+    if (start_index == null) {
+      start_index = 0;
+    }
+
+    for (var note = 0; note < 12; note++) {
+      var out = document.getElementsByClassName("note_" + note);
+      note_in_key = note + start_index;
+      for (var i = 0; i < out.length; i++) {
+        if (notes[note_in_key].length != 0) {
+          out[i].innerHTML = notes[note_in_key][0];
+        } else {
+          out[i].innerHTML = notes[note][0];
+        }
+      }
+    }
+  }
+}
